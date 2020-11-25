afca8aff7c39b77000a8d4909290c129db5393f7
Omri Levy <omri2861@gmail.com>
2020-11-25 22:21:39 +0200

    Replaced buttons with Icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
-import { Button, Grid, Box } from "@material-ui/core";
+import { Button, Grid, Box, IconButton } from "@material-ui/core";
+import ToysRoundedIcon from '@material-ui/icons/ToysRounded';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
 
 function App() {
     // TODO: Find a better way to align content to the middle
@@ -8,19 +12,19 @@ function App() {
             <Box>
                 <Grid container spacing="1">
                     <Grid item>
-                        <Button variant="contained">FAN</Button>
+                        <Button variant="contained"><ToysRoundedIcon /></Button>
                     </Grid>
                     <Grid item>
                         <Button variant="contained">MODE</Button>
                     </Grid>
                     <Grid item>
-                        <Button variant="contained">PLUS</Button>
+                        <Button variant="contained"><AddIcon /></Button>
                     </Grid>
                     <Grid item>
-                        <Button variant="outlined">MINUS</Button>
+                        <Button variant="contained"><RemoveIcon /></Button>
                     </Grid>
                     <Grid item md="2">
-                        <Button variant="outlined">ON/OFF</Button>
+                        <Button variant="contained" color="secondary"><PowerSettingsNewIcon /></Button>
                     </Grid>
                 </Grid>
             </Box>
