85bcb5d4e727d0406668d0870916234bd26abc32
Omri Levy <omri2861@gmail.com>
2020-11-25 22:34:55 +0200

    Aligned buttons like the real AC remote

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
-import { Button, Grid, Box, IconButton } from "@material-ui/core";
+import { Button, Grid, Box} from "@material-ui/core";
 import ToysRoundedIcon from '@material-ui/icons/ToysRounded';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
+
+/* This componenet creates space for the button grid 
+ * props.cells - The amount of cells to space
+ */
+function GridSpace(props) {
+
+    return <Grid item xs={props.cells}><Box /></Grid>
+}
 
 function App() {
     // TODO: Find a better way to align content to the middle
@@ -11,19 +19,20 @@ function App() {
         <div className="App">
             <Box>
                 <Grid container spacing="1">
-                    <Grid item>
+                    <Grid item xs={4}>
                         <Button variant="contained"><ToysRoundedIcon /></Button>
                     </Grid>
-                    <Grid item>
+                    <Grid item xs={4}>
                         <Button variant="contained">MODE</Button>
                     </Grid>
-                    <Grid item>
+                    <Grid item xs={4}>
                         <Button variant="contained"><AddIcon /></Button>
                     </Grid>
-                    <Grid item>
+                    <GridSpace cells={8} />
+                    <Grid item xs={4}>
                         <Button variant="contained"><RemoveIcon /></Button>
                     </Grid>
-                    <Grid item md="2">
+                    <Grid item xs={12}>
                         <Button variant="contained" color="secondary"><PowerSettingsNewIcon /></Button>
                     </Grid>
                 </Grid>
