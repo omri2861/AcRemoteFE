e952aa094be6acbc9d7437598e5fe12c4678f22a
Omri Levy <omri2861@gmail.com>
2020-11-30 14:29:42 +0200

    Removed redundant import

--- src/acModes.jsx
+++ src/acModes.jsx
@@ -1,17 +1,9 @@
-import React from 'react';
-
 import ChangeHistoryIcon from '@material-ui/icons/ChangeHistory';
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
 import OpacityIcon from '@material-ui/icons/Opacity';
 import ToysIcon from '@material-ui/icons/Toys';
 import AcUnitIcon from '@material-ui/icons/AcUnit';
 
-const modes = [
-  ChangeHistoryIcon,
-  AcUnitIcon,
-  WbSunnyIcon,
-  ToysIcon,
-  OpacityIcon,
-];
+const modes = [ChangeHistoryIcon, AcUnitIcon, WbSunnyIcon, ToysIcon, OpacityIcon];
 
 export default modes;
